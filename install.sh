6ff7316c6991a3de48a9919bddeb6542b7016913
hmallen <allenhm@gmail.com>
2018-02-17 20:33:42 +0000

    Fixed install order for TA-Lib function.

--- install.sh
+++ install.sh
@@ -7,13 +7,15 @@ cd src &&
 wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz &&
 tar xvf ta-lib-0.4.0-src.tar.gz &&
 cd ta-lib &&
-./configure &&
+./configure --prefix=/usr &&
 make &&
 sudo make install &&
 cd ../.. &&
 sudo yum install python36 python36-virtualenv -y &&
 python3 -m virtualenv env &&
 source env/bin/activate &&
+pip install numpy &&
+pip install TA-Lib &&
 pip install git+https://github.com/s4w3d0ff/python-poloniex.git &&
 pip install -r requirements.txt &&
 echo "Done!"
