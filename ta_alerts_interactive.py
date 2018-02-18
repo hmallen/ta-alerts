ae5ecda77896f492c4daaac3316c3cc01f09ab13
hmallen <allenhm@gmail.com>
2018-02-17 22:20:35 -0500

    Working on restart bin size fix.

--- ta_alerts_interactive.py
+++ ta_alerts_interactive.py
@@ -726,7 +726,9 @@ if __name__ == '__main__':
             for market in ta_users[user]:
                 for indicator in ta_users[user][market]:
                     for bin_size in ta_users[user][market][indicator]:
+                        logger.debug('bin_size: ' + bin_size)
                         temp_bin_size = int(bin_size)
+                        logger.debug('temp_bin_size: ' + str(temp_bin_size))
                         temp_dict = ta_users[user][market][indicator][bin_size]
                         del ta_users[user][market][indicator][bin_size]
                         ta_users[user][market][indicator][temp_bin_size] = temp_dict
